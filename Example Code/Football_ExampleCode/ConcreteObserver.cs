d19de76437df6237295e5c3cbfcf8c74e927be57
Pablo <pedrodelpiedro@gmail.com>
2025-12-04 10:20:31 +0100

    Create and implement an implementation of the Interface

---
+++ Example Code/Football_ExampleCode/ConcreteObserver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_ExampleCode
+{
+    internal class ConcreteObserver : IObserver
+    {
+        public int amountOfGoals { get; set; } = 0;
+
+        public string name { get; set; }
+
+        public string rolle {  get; set; }
+
+        public void Update(int value) 
+        {
+            amountOfGoals = amountOfGoals + value;
+        }
+
+        public ConcreteObserver (string name, string rolle) 
+        { 
+            this.name = name;   
+            this.rolle = rolle;
+        }
+    }
+}
