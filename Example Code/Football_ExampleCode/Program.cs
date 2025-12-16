893f0da3940b5c972d33283bf5c9cd44ab6e5c7c
Pablo <pedrodelpiedro@gmail.com>
2025-12-16 10:07:02 +0100

    Exchange into football team example

--- Example Code/Football_ExampleCode/Program.cs
+++ Example Code/Football_ExampleCode/Program.cs
@@ -10,47 +10,44 @@ namespace Football_ExampleCode
             ConcreteObserver Simona = new ConcreteObserver("Simona", "Mutter");
             ConcreteObserver Jonas = new ConcreteObserver("Jonas", "Bruder");
 
-            Subject David = new Subject("David", "Stürmer");
+            Subject Manchester_United = new Subject("Bruno Fernandes", "Stürmer");
 
             Console.WriteLine("Familienmitglieder, welche kommen, werden aufgeschrieben");
-            David.Attach(Manuell);
-            David.Attach(Simona);
-            David.Attach(Jonas);
+            Manchester_United.Attach(Manuell);
+            Manchester_United.Attach(Simona);
+            Manchester_United.Attach(Jonas);
 
             Console.WriteLine("\n--------------------------");
-            Console.WriteLine("Notierte Goals (Vor Spiel)");
-            foreach (ConcreteObserver Person in David.Observers)
+            Console.WriteLine("Benachrichtige Goals (Vor Spiel)");
+            foreach (ConcreteObserver Person in Manchester_United.Observers)
                 Console.WriteLine($"{Person.rolle} {Person.name}: {Person.amountOfGoals}");
             Console.WriteLine("--------------------------");
 
 
             Console.WriteLine("\nJonas kommt doch nicht mit");
             Console.WriteLine("\nJonas wird aus Liste entfernt");
-            David.Detach(Jonas);
+            Manchester_United.Detach(Jonas);
 
-            Console.WriteLine("\nDavid hat ein Goal geschossen");
-            David.GoalSchiessen();
-            Console.WriteLine($"Anzahl Tore: {David.amountOfGoals}");
+            Console.WriteLine($"\n{Manchester_United.name} hat ein Goal geschossen");
+            Manchester_United.GoalSchiessen();
+            Console.WriteLine($"Anzahl Tore: {Manchester_United.amountOfGoals}");
 
             Console.WriteLine("\n--------------------------");
-            Console.WriteLine("Notierte Goals (Während Spiel)");
-            foreach (ConcreteObserver Person in David.Observers)
+            Console.WriteLine("Benachrichtige Goals (Während Spiel)");
+            foreach (ConcreteObserver Person in Manchester_United.Observers)
                 Console.WriteLine($"{Person.rolle} {Person.name}: {Person.amountOfGoals}");
             Console.WriteLine("--------------------------");
 
-            Console.WriteLine("\nJonas kommt doch noch");
-            Console.WriteLine("\nJonas wird zur Liste hinzugefügt");
-            David.Attach(Jonas);
-
-            Console.WriteLine("\nDavid hat ein Goal geschossen");
-            David.GoalSchiessen();
-            Console.WriteLine($"Anzahl Tore: {David.amountOfGoals}");
+            Console.WriteLine($"\n{Manchester_United.name} hat ein Goal geschossen");
+            Manchester_United.GoalSchiessen();
+            Console.WriteLine($"Anzahl Tore: {Manchester_United.amountOfGoals}");
 
             Console.WriteLine("\n--------------------------");
-            Console.WriteLine("Notierte Goals (Während Spiel)");
-            foreach (ConcreteObserver Person in David.Observers)
+            Console.WriteLine("Benachrichtige Goals (Während Spiel)");
+            foreach (ConcreteObserver Person in Manchester_United.Observers)
                 Console.WriteLine($"{Person.rolle} {Person.name}: {Person.amountOfGoals}");
             Console.WriteLine("--------------------------");
+
         }
     }
 }
